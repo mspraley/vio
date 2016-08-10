1960296e2485225198eebd9b95ced63dd60c1ee0
Matt Spraley <Matt@MATTHEWs-MacBook-Pro.local>
2016-08-10 18:32:53 -0400

    initial

---
+++ app/components/Notes.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Note from './Note';
+
+export default({notes}) => (
+    <ul>{notes.map(note =>
+        <li key={note.id}><Note task={note.task}/></li>  
+    )}
+    </ul>
+)
